bf5d9fdd1247c92d5dd94cc2bc8456766216e534
martincz <martincz@mokeedev.com>
2018-01-15 09:12:52 +0800

    base: Call log delete limit (2/2)
    Change-Id: Ie91a75742186f855c7ff0fd9d0a2dc98e3f836f2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4187,6 +4187,11 @@ public final class Settings {
         };
 
         /**
+         * Call log delete limit
+         */
+        public static final String CALL_LOG_DELETE_LIMIT = "call_log_delete_limit";
+
+        /**
          * Setting to determine whether or not to show the battery percentage in the status bar.
          *    0 - Don't show percentage
          *    1 - Show percentage outside the battery icon
@@ -4199,7 +4204,7 @@ public final class Settings {
         private static final Validator SHOW_BATTERY_PERCENT_VALIDATOR =
                 new SettingsValidators.InclusiveIntegerRangeValidator(0, 2);
 
-	/**
+        /**
          * show the membar in recents
          *  @hide
          */
